68847ef5fa3b09d99a30f2c655cad7cc805ea67f
manikanta7070 <139975964+manikanta7070@users.noreply.github.com>
2023-08-14 19:51:44 +0530

    Add files via upload
    comparision operators

---
+++ comparision operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 3;
+  printf("%d", x == y); 
+  printf("%d", x != y);
+  printf("%d", x > y); 
+  printf("%d", x < y);
+  printf("%d", x >= y);
+  printf("%d", x <= y);
+  return 0;
+}
