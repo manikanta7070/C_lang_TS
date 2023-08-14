d69a8246687fd2bcdde8ca52fe09ebbb112a9f45
manikanta7070 <139975964+manikanta7070@users.noreply.github.com>
2023-08-14 19:52:15 +0530

    logical operators

---
+++ logical operators.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  int y = 3;
+  printf("%d", x > 3 && x < 10);
+  printf("%d", x > 3 || x < 4);
+   printf("%d", !(x > 3 && x < 10));
+  return 0;
+}
