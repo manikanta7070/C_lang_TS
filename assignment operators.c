17a2b3f14c9ca49bfeaf4f464d22175cde43e73c
manikanta7070 <139975964+manikanta7070@users.noreply.github.com>
2023-08-14 19:50:59 +0530

    Add files via upload
    assign operator

---
+++ assignment operators.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  printf("%d", x);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  x += 3;
+  printf("%d", x);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  x -= 3;
+  printf("%d", x);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  x *= 3;
+  printf("%d", x);
+  return 0;
+}
+
+#include <stdio.h>
+
+int main() {
+  float x = 5;
+  x /= 3;
+  printf("%f", x);
+  return 0;
+}
+
+
+#include <stdio.h>
+
+int main() {
+  int x = 5;
+  x >>= 3;
+  printf("%d", x);
+  return 0;
+}
+
+#include <stdio.h>
+int main() {
+  int x = 5;
+  x <<= 3;
+  printf("%d", x);
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
